d5a41522e0df57b5d65cc94eafa75545c2235d43
afrokick <devbyru@gmail.com>
2022-01-04 12:29:47 +0300

    init

---
+++ docs/api/objects/image.md
@@ -0,0 +1,11 @@
+---
+sidebar_position: 2
+---
+
+# Изображение
+
+Let's translate `docs/intro.md` to French.
+
+## Configure i18n
+
+Modify `docusaurus.config.js` to add support for the `fr` locale:
---
+++ docusaurus.config.js
@@ -0,0 +1,60 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Edik',
+  tagline: 'itgenio',
+  url: 'https://geditor.eu.meteorapp.com',
+  baseUrl: '/docs/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.png',
+  organizationName: 'itgenio', // Usually your GitHub org/user name.
+  projectName: 'geditor-docs', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/itgenio/geditor',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Edik',
+        logo: {
+          alt: 'Edik logo',
+          src: 'img/favicon.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Документация',
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
